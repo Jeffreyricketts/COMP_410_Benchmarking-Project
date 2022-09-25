579d238cbbfa09d2da995697d5c84a43763d14f9
Jeffreyricketts <rickjeff44@gmail.com>
2022-09-25 16:50:02 -0500

    Data obtained

--- triviaKernelCalls/build.bash
+++ triviaKernelCalls/build.bash
@@ -2,7 +2,11 @@
 gcc -o getpidBenchmark getpidBenchmark.c
 gcc -o getcwdBenchmark getcwdBenchmark.c
 gcc -o getppidBenchmark getppidBenchmark.c
+gcc -o getcpuBenchmark getcpuBenchmark.c
+gcc -o getcurrentdirnameBenchmark getcurrentdirnameBenchmark.c
 
+./getcpuBenchmark > ../data/getcpuBenchmark.csv
+./getcurrentdirnameBenchmark > ../data/getcurrentdirnameBenchmark.csv
 ./getpidBenchmark > ../data/getpid.csv
 ./getcwdBenchmark > ../data/getcwdBenchmark.csv
 ./getppidBenchmark > ../data/getppidBenchmark.csv
